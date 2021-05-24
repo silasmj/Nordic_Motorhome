276fda9d11e61393272a8fd9effaf841e2350526
silas@icloud.com <smjsmj123>
2021-05-24 16:00:05 +0200

    Changes by Oliver Petersen

--- src/main/java/com/example/nordic_motorhome/Repository/MotorhomeRepo.java
+++ src/main/java/com/example/nordic_motorhome/Repository/MotorhomeRepo.java
@@ -30,16 +30,18 @@ public class MotorhomeRepo {
         String sql = "DELETE FROM motorhome WHERE motorhome_id = ?";
         return template.update(sql, motorhome_id) < 0;
     }
-    public Motorhome updateMotorhome(int motorhome_id, Motorhome m){
-        String sql = "UPDATE motorhome SET brand = ?, model = ?, type = ? WHERE motorhome_id = ?";
-        template.update(sql, m.getBrand(), m.getModel(), m.getType());
-        return null;
-    }
+
     public Motorhome findMotorhomeById(int motorhome_id){
         String sql = "SELECT * FROM motorhome WHERE motorhome_id = ?";
         RowMapper<Motorhome> rowMapper = new BeanPropertyRowMapper<>(Motorhome.class);
         Motorhome m = template.queryForObject(sql, rowMapper, motorhome_id);
         return m;
+    }
+
+    public Motorhome updateMotorhome(int motorhome_id, Motorhome m){
+        String sql = "UPDATE motorhome SET brand = ?, model = ?, type = ? WHERE motorhome_id = ?";
+        template.update(sql, m.getBrand(), m.getModel(), m.getType(), m.getMotorhome_id());
+        return null;
     }
 }
 
